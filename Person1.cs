c67c3163dd82eeb8714825d2ed96a5c1441bce12
Youssef Nabeh <115867801+yousseffe@users.noreply.github.com>
2024-07-04 20:05:05 +0300

    Q2:Struct for Person and Displaying Details

---
+++ Person1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_1_OOP
+{
+    internal struct Person1
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Person1(String _name, int _age)
+        {
+            name = _name;
+            age = _age;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -21,6 +21,19 @@
                 Console.WriteLine(day);
             }*/
             #endregion
+
+            #region 2-Struct for Person and Displaying Details
+            /*Person[] persons = new Person[3];
+            persons[0] = new Person("Alice", 25);
+            persons[1] = new Person("Bob", 30);
+            persons[2] = new Person("Charlie", 28);
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person);
+            }*/
+            #endregion
+
         }
     }
 }
