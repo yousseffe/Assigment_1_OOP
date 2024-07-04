68e37bad5ab9c07840da81398defb49669b31339
Youssef Nabeh <115867801+yousseffe@users.noreply.github.com>
2024-07-04 20:05:52 +0300

    Q3:Enum for Season and Displaying Month Range

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@
         Saturday,
         Sunday
     }
-
+    enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -31,6 +37,38 @@
             foreach (Person person in persons)
             {
                 Console.WriteLine(person);
+            }*/
+            #endregion
+
+            #region 3-Enum for Season and Displaying Month Range
+            /*Console.Write("Enter season name: ");
+            string input = Console.ReadLine();
+            Season season;
+
+            if (Enum.TryParse(input, true, out season))
+            {
+                switch (season)
+                {
+                    case Season.Spring:
+                        Console.WriteLine("March to May");
+                        break;
+                    case Season.Summer:
+                        Console.WriteLine("June to August");
+                        break;
+                    case Season.Autumn:
+                        Console.WriteLine("September to November");
+                        break;
+                    case Season.Winter:
+                        Console.WriteLine("December to February");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid season");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid season");
             }*/
             #endregion
 
