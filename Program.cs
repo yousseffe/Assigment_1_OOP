c74843ea89520bacd2cd32e3673baa584a4d1ff3
Youssef Nabeh <115867801+yousseffe@users.noreply.github.com>
2024-07-04 20:03:12 +0300

    Q1:Enum for WeekDays and Printing Days of the Week

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace Assigment_1_OOP
 {
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region 1-Enum for WeekDays and Printing Days of the Week
+            /*foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }*/
+            #endregion
         }
     }
 }
